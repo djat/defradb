8ce4bb2d4c3b23af5aeccae3c8f6d3e69cd571b4
David Thomson <djathomson@Davids-MacBook-Pro.local>
2023-04-24 12:49:01 -0700

    Additional changes to load schemas from a file

--- tools/docker/run_defradb_create.sh
+++ tools/docker/run_defradb_create.sh
@@ -8,19 +8,22 @@ echo $PID_DEFRA
 
 echo "Process 1 lasts for 5s" && sleep 5 &
 
-
 PID=$!
 
-#sleep 50 &
 
 wait $PID
 
-/defradb client schema add --url 0.0.0.0:9181 'type Article {    content: String    published: Boolean  }'  > ~/schema_output.txt 
+/defradb client schema add --url 0.0.0.0:9181 -f /usr/defra_examples/schema/user.graphql  
 
-echo "Process 2 lasts for 5s" && sleep 5 &
+DIP=$!
+
+echo "Process 2 lasts for 10s" && sleep 10 &
+
+wait $DIP
+
 
 kill $PID_DEFRA
 
-echo "Process 3 lasts for 10s" && sleep 10 &
+
 
 /defradb start --url 0.0.0.0:9181
